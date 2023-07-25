a01c2c333c120fcf21f74f1fe3709944a3361873
Fcoelhojnp <fcoelho.jnp@gmail.com>
2023-07-25 13:01:11 -0300

    adicionando vercel.json

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ const cors = require("cors");
 
 const app = express();
 
+//Middleware JSON
+app.use(express.json());
+
+//Middleware JSON
+app.use(cors());
+
+// Rotas
+app.get("/retorno", function(request, response){
+    response.status(200).json({retorno: "Confirmado"});
+});
+
 app.listen(3000, function(){
     console.log("Iniciado Porta 3000");
 
